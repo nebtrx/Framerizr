ab796d4d0a4e2dc04b7aa18d6eb9ccd632b5e353
omar <omar@localhost>
2013-06-22 03:42:41 -0400

    Framerizr: Initial Commit. May the force be with U.;)

---
+++ Framerizr/Scripts/framerizr-1.0.0.js
@@ -0,0 +1,53 @@
+﻿(function ($, window) {
+    "use strict";
+
+    if (typeof($) !== "function") {
+        // no jQuery!
+        throw new Error("Framerizr: jQuery not found. Please ensure jQuery is referenced before the framerizr.js file.");
+    }
+
+    function getOrigin(href) {
+        var a = document.createElement("a");
+        a.href = href;
+        return a.origin;
+    };
+
+
+    var framerizr = {
+        start: function() {
+            $("[data-iframe-src]").each(function() {
+                var $container = $(this);
+                var iframeSrc = $container.data('iframe-src');
+
+                // if data-iframe-src attr present
+                if (iframeSrc != null || iframeSrc != "") {
+
+                    var remote = "";
+                    var framerizrUri = $container.data('framerizr-uri');
+                    if (framerizrUri == null || framerizrUri == "") {
+                        remote = getOrigin(iframeSrc) + "/Framerizr?uri=" + iframeSrc;
+                    } else {
+                        remote = framerizrUri + "?uri=" + iframeSrc;
+                    }
+
+                    new easyXDM.Socket({
+                        remote: remote,
+                        container: $container[0],
+                        onMessage: function(message, origin) {
+                            // resizing
+                            $container.find('iframe').height(message + "px");
+                            $container.height(message + "px");
+                        }
+                    });
+                }
+            });
+
+        }
+    };
+    
+    $.framerizr = framerizr;
+}(window.jQuery, window));
+
+(function ($) {
+    $.framerizr.start();
+}(window.jQuery));
---
+++ Framerizr/pre-processed/Index.cshtml.pp
@@ -0,0 +1,29 @@
+﻿@model Uri
+@{
+    Layout = null;
+}
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>@ViewBag.Title</title>
+    <style type="text/css">
+        html {
+            height: 100%;
+        }
+
+        body {
+            margin: 0;
+            height: 100%;
+            overflow: hidden;
+        }
+    </style>
+</head>
+<body>
+    <iframe onload="$.framerizr.server.start()" frameborder = 0 scrolling="no" src="@Model" style="width: 100%; height: 100%"></iframe>
+	<script src="@Url.Content("~/Scripts/jquery-1.6.2.min.js")">  </script>
+    <script src="@Url.Content("~/Scripts/easyXDM-2.4.17.1.min.js")">  </script>
+    <script src="@Url.Content("~/Scripts/framerizr.server-1.0.0.js")">  </script>
+</body>
+</html>
