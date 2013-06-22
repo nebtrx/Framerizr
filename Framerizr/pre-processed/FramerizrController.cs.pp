6dc1d79cb0b95ddf8df67d6028bbab4f930f1478
Omar Ahmed <neb.trx@gmail.com>
2013-06-22 03:42:41 -0400

    Framerizr: Initial Commit. May the force be with U.;)

---
+++ Framerizr/pre-processed/FramerizrController.cs.pp
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace $rootnamespace$.Controllers
+{
+    public class FramerizrController : Controller
+    {
+        //
+        // GET: /Framerizr/
+
+        public ActionResult Index(Uri uri)
+        {
+            return View(uri);
+        }
+    }
+}
