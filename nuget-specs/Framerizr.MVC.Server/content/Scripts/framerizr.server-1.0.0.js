90550cf77a39437289da33af285ee6d84b2dd880
Omar Ahmed <neb.trx@gmail.com>
2013-06-22 18:30:20 -0400

    Changes to support the README.txt Logo 32x32 Updated.

---
+++ nuget-specs/Framerizr.MVC.Server/content/Scripts/framerizr.server-1.0.0.js
@@ -0,0 +1,47 @@
+﻿(function ($, window) {
+    "use strict";
+
+    if (typeof ($) !== "function") {
+        // no jQuery!
+        throw new Error("Framerizr: jQuery not found. Please ensure jQuery is referenced before the framerizr.js file.");
+    }
+
+    var framerizr = $.framerizr;
+
+    if (!framerizr) {
+        $.framerizr = framerizr = {};
+    }
+
+    framerizr.server = {
+        socket: {},
+
+        pushData: function () {
+            framerizr.server.socket.postMessage($("iframe").contents().find('body').height() + 20);
+        },
+
+        start: function () {
+            framerizr.server.socket = new easyXDM.Socket({
+                onReady: function () {
+                    framerizr.server.pushData();
+                }
+            });
+
+            MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+            var observer = new MutationObserver(function (mutations, observer) {
+                // fired when a mutation occurs
+
+                setTimeout(function () {
+                    framerizr.server.pushData();
+                }, 500);
+            });
+
+            // define what element should be observed by the observer
+            // and what types of mutations trigger the callback
+            observer.observe(document.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("body")[0], {
+                subtree: true,
+                attributes: true
+            });
+        },
+    };
+}(window.jQuery, window));
